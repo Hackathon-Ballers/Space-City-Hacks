ea2ec431d3ce6a05e819fe3ceed7b582d363ff2b
Joey Marcinowski <jmarcinowski@hillers.org>
2022-06-19 12:06:37 -0400

    peenis

--- TruckProject/App.tsx
+++ TruckProject/App.tsx
@@ -1,25 +1,89 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Component } from 'react'
+import Sound from 'react-native-sound';
+
+var globalTime = 10;
+var startTime = 0;
 
 class App extends Component {
   constructor(props) {
     super(props)
-    this.state = { heartRate: 0 }
+    this.state = {
+      heartRate: 0,
+      calibrating: true,
+      count: 0,
+      totalHeartBeat: 0,
+      avgHeartBeat: 0,
+      occurrencesUnderThreshold: 0,
+      panic: false,
+      panicSound: null
+    }
   }
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>{this.state.heartRate}</Text>
-        <StatusBar style="auto" />
-      </View>
-    );
+    if (this.state.calibrating === false && !this.state.panic) {
+      return (
+        <View style={styles.container}>
+          <Text>{this.state.heartRate}</Text>
+          <StatusBar style="auto" />
+        </View>
+      );
+    }
+    else if (this.state.calibrating && !this.state.panic) {
+      return (
+        <View style={styles.container}>
+          <Text>Calibrating {globalTime - this.state.count}</Text>
+        </View>
+      )
+    } else if (this.state.panic) {
+      return (
+        <View style={{ backgroundColor: 'red', height: 500, alignItems: 'center', justifyContent: 'center' }}>
+          <Text>PANIC PANIC</Text>
+        </View>
+      )
+    }
   }
   getHeartRate = async () => {
     // If you're using the fake development server, use that URL HERE!
-    let response = await fetch("http://192.168.1.33").then(response => response.json())
+    this.setState((prevState, props) => ({
+      count: prevState.count + 1
+    }));
+    let response = await fetch("http://192.168.1.167").then(response => response.json())
     this.setState({ heartRate: response['bpm'] })
+    console.log(this.state.count)
+    console.log("hr: " + this.state.heartRate);
+    if (this.state.count > startTime && this.state.count < globalTime) {
+      this.getAverage()
+    } else if (this.state.count >= globalTime) {
+      this.setState({ calibrating: false })
+      let threshold = this.state.avgHeartBeat - 6
+      console.log("threshold", threshold)
+      if (this.state.heartRate <= threshold) {
+        console.log("Under threshold")
+        this.setState({ occurrencesUnderThreshold: this.state.occurrencesUnderThreshold + 1 })
+      }
+      else {
+        this.setState({ occurrencesUnderThreshold: 0 })
+      }
+      console.log("under threshold: ", this.state.occurrencesUnderThreshold)
+    }
+
+    if (this.state.occurrencesUnderThreshold > 10) {
+      this.setState({ panic: true })
+    }
   }
+
+  getAverage() {
+    let totalHeartBeats = this.state.totalHeartBeat
+    let count = this.state.count
+    let calcAvg = totalHeartBeats / count
+    this.setState((prevState, props) => ({
+      totalHeartBeat: prevState.totalHeartBeat + prevState.heartRate
+    }))
+    console.log("avg " + this.state.totalHeartBeat / (globalTime - startTime))
+    this.setState({ avgHeartBeat: this.state.totalHeartBeat / (globalTime - startTime) });
+  }
+
   componentDidMount() {
     console.log("mounted")
     setInterval(this.getHeartRate, 1000)
